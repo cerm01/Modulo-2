10acf0e0a4d1aa056d7e4cefe92a9de923255fee
CERM <carlos.rosas7476@alumnos.udg.mx>
2023-10-06 00:22:04 -0600

    Modificacion de los nuevos archivos para guardar cliente

--- src/main/java/com/mycompany/archivos_secuenciales/cliente.java
+++ src/main/java/com/mycompany/archivos_secuenciales/cliente.java
@@ -2,5 +2,42 @@
 package com.mycompany.archivos_secuenciales;
 
 public class cliente {
+
+    private int id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
     
 }
